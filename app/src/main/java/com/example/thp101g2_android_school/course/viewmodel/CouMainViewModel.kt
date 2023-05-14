73368aabc05d1d101e7e03534b73e43628ca9a4d
Leo <leoyuan17@gmail.com>
2023-05-14 10:26:32 +0800

    課程 專案初始化

---
+++ app/src/main/java/com/example/thp101g2_android_school/course/viewmodel/CouMainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.thp101g2_android_school.course.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class CouMainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
