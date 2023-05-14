b243ad8aa6d74483614f0ddeaa91abb9b051e5c8
River <shinshigobi@gmail.com>
2023-05-14 11:07:40 +0800

    會員、日曆 初始化

---
+++ app/src/main/java/com/example/thp101g2_android_school/calendar/model/Calendar.kt
@@ -0,0 +1,4 @@
+package com.example.thp101g2_android_school.calendar.model
+
+class Calendar {
+}
---
+++ app/src/main/java/com/example/thp101g2_android_school/member/viewModel/MemMainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.thp101g2_android_school.member.viewModel
+
+import androidx.lifecycle.ViewModel
+
+class MemMainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
