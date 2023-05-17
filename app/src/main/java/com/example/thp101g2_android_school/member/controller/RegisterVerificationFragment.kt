be9a2c497d2ce05bee1eeeeae87593290796371b
River <shinshigobi@gmail.com>
2023-05-17 18:36:41 +0800

    登入、註冊、日曆layout

--- app/src/main/java/com/example/thp101g2_android_school/member/controller/RegisterVerificationFragment.kt
+++ app/src/main/java/com/example/thp101g2_android_school/member/controller/RegisterVerificationFragment.kt
@@ -1,4 +1,4 @@
-package com.example.thp101g2_android_school.calendar.controller
+package com.example.thp101g2_android_school.member.controller
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
@@ -6,27 +6,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.thp101g2_android_school.calendar.viewModel.CalMainViewModel
 import com.example.thp101g2_android_school.R
+import com.example.thp101g2_android_school.member.viewModel.RegisterVerificationViewModel
 
-class CalMainFragment : Fragment() {
+class RegisterVerificationFragment : Fragment() {
 
     companion object {
-        fun newInstance() = CalMainFragment()
+        fun newInstance() = RegisterVerificationFragment()
     }
 
-    private lateinit var viewModel: CalMainViewModel
+    private lateinit var viewModel: RegisterVerificationViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_cal_main, container, false)
+        return inflater.inflate(R.layout.fragment_register_verification, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(CalMainViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(RegisterVerificationViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
