bd6f13c505c958178c8607eba205233f82f245d8
Grace <wl03191776@gmail.com>
2023-05-19 11:44:20 +0800

    廠商首頁更新(recyclerView)

---
+++ app/src/main/java/com/example/thp101g2_android_school/firm/model/FirmProduct.kt
@@ -0,0 +1,6 @@
+package com.example.thp101g2_android_school.firm.model
+
+import java.io.Serializable
+
+class FirmProduct(var imageId: Int, var name: String, var author: String,  var price: String, var pinfo: String):
+    Serializable
