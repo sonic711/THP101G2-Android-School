182abeb057433f19529ffa71724db868635d3cc2
Sean <uanex711@gmail.com>
2023-05-19 15:47:50 +0800

    社群 po文頁面layout更新

--- app/src/main/java/com/example/thp101g2_android_school/community/viewmodel/ComPostViewModel.kt
+++ app/src/main/java/com/example/thp101g2_android_school/community/viewmodel/ComPostViewModel.kt
@@ -4,6 +4,17 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ComPostViewModel : ViewModel() {
+    val memberName: MutableLiveData<String> by lazy { MutableLiveData<String>() }
+    val memberImg: MutableLiveData<Int> by lazy { MutableLiveData<Int>() }
+    val postTime: MutableLiveData<String> by lazy { MutableLiveData<String>() }
+    val secClass: MutableLiveData<String> by lazy { MutableLiveData<String>() }
+    val private: MutableLiveData<Boolean> by lazy { MutableLiveData<Boolean>() }
     val title: MutableLiveData<String> by lazy { MutableLiveData<String>() }
     val content: MutableLiveData<String> by lazy { MutableLiveData<String>() }
+    init {
+        loadData()
+    }
+    private fun loadData(){
+
+    }
 }
