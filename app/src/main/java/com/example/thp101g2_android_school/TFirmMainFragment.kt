8ad7a686091e59fc90a48e814fdbeadf92263fd2
Sean <uanex711@gmail.com>
2023-05-20 15:24:59 +0800

    建立四個Activity

---
+++ app/src/main/java/com/example/thp101g2_android_school/TFirmMainFragment.kt
@@ -0,0 +1,11 @@
+package com.example.thp101g2_android_school
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class TFirmMainFragment : Fragment() {
+
+}
